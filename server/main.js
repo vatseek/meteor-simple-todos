ecac4b07a3feba7ad93d467ee3a3cb596ac8e2f4
vatseek <vadim.gumennyj@gmail.com>
2017-03-23 16:30:23 +0200

    step3

--- server/main.js
+++ server/main.js
@@ -1,4 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+export const Tasks = new Mongo.Collection('tasks');
 
 Meteor.startup(() => {
   // code to run on server at startup
