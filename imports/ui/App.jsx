a7a9e6bbddf003b17aaf20a0cffaffa78c67501b
vatseek <vadim.gumennyj@gmail.com>
2017-03-24 09:06:41 +0200

    step4

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
 import {createContainer} from 'meteor/react-meteor-data';
 
 import {Tasks} from '../api/tasks.js';
@@ -12,12 +13,32 @@ class App extends Component {
     ));
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
         </header>
+
+        <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            ref="textInput"
+            placeholder="Type to add new tasks"
+          />
+        </form>
 
         <ul>
           {this.renderTasks()}
@@ -33,6 +54,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
